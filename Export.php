de5c62174eec97d80296e4b1ea4580a197a2a806
minhthanh1412 <40221777+minhthanh1412@users.noreply.github.com>
2018-07-03 08:16:14 +0700

    Add files via upload

--- Export.php
+++ Export.php
@@ -3,26 +3,69 @@ require("connectDB.php");
 require("Classes/PHPExcel.php");
 if(isset($_POST['btnExport']))
 {
+	$maCT =$_POST["CTDT"];
+	$sqlTenCT = 'SELECT * FROM tblchuongtrinhdt WHERE maCT = '.$maCT;
+	$rl = $conn->query($sqlTenCT);
+	while ($rowCT = mysqli_fetch_array($rl)) {
+		$tenCT = $rowCT["tenCT"];
+	}
+
 	$objExcel = new PHPExcel;
 	$objExcel -> setActiveSheetIndex(0);
 	$sheet = $objExcel -> getActiveSheet() -> setTitle("Monhoc");
 	$rowCount =1;
-	$sheet -> setCellValue('A'.$rowCount, "Mã Ngành");
-	$sheet-> setCellValue('B'.$rowCount, "Tên ngành");
+	//Ten Chương trình
+	$sheet -> setCellValue('A'.$rowCount, "$tenCT");
+	$sheet->mergeCells("A".($rowCount).":F".($rowCount));
 	$sheet -> getColumnDimension("B") ->setAutoSize(true);
-	$sheet ->getStyle('A1:B1') -> getFill() -> setFillType(\PHPExcel_Style_Fill::FILL_SOLID) -> getStartColor() ->setARGB('00ffff00');
-	$sheet->getStyle('A1:B1')->getAlignment() ->setHorizontal(PHPExcel_Style_Alignment::HORIZONTAL_CENTER);
+	$sheet ->getStyle('A1:F1') -> getFill() -> setFillType(\PHPExcel_Style_Fill::FILL_SOLID) -> getStartColor() ->setARGB('00ffff00');
+	$sheet->getStyle('A1:F1')->getAlignment() ->setHorizontal(PHPExcel_Style_Alignment::HORIZONTAL_CENTER);
 
-	$sql = "SELECT * FROM tblnganh";
-	$result = $conn -> query($sql);
+	// //In ra từng học kì
+	$rowCount = $rowCount+1;
+	for($i=1; $i<=8; $i++)
+	{ 
+		// $sheet ->getStyle('A'.$rowCount) -> getFill() -> setFillType(\PHPExcel_Style_Fill::FILL_SOLID) -> getStartColor() ->setARGB('0099ff');
+		$sheet->getStyle('A'.$rowCount)->getAlignment() ->setHorizontal(PHPExcel_Style_Alignment::HORIZONTAL_CENTER);
 
-	while ($row = mysqli_fetch_array($result) ) {
+		$sheet -> setCellValue('A'.$rowCount, "Học kì ". $i);
+		$sheet->mergeCells("A".($rowCount).":F".($rowCount));
 		$rowCount++;
-		$sheet -> setCellValue('A'.$rowCount, $row['maNganh']);
-		$sheet-> setCellValue('B'.$rowCount, $row['tenNganh']);
+		$sheet ->getStyle('A'.$rowCount.':F'.$rowCount) -> getFill() -> setFillType(\PHPExcel_Style_Fill::FILL_SOLID) -> getStartColor() ->setARGB('33CCFF');
+		$sheet->getStyle('A'.$rowCount.':F'.$rowCount)->getAlignment() ->setHorizontal(PHPExcel_Style_Alignment::HORIZONTAL_CENTER);
+		$tam =$rowCount;
+		$sheet -> setCellValue('A'.$rowCount, "id");
+		$sheet-> setCellValue('B'.$rowCount, "Tên môn học");
+		$sheet -> setCellValue('C'.$rowCount, "Tổng STC");
+		$sheet-> setCellValue('D'.$rowCount, "Lý thuyết");
+		$sheet -> setCellValue('E'.$rowCount, "Thực hành");
+		$sheet-> setCellValue('F'.$rowCount, "Bắt buộc");
+		
+		$sql = "SELECT id, tenMon, tongSoTC, soTCLyThuyet, soTCThuchanh, batbuoc FROM tblmonhoc, tblchitietctdt WHERE tblmonhoc.id = tblchitietctdt.maMH AND tblchitietctdt.soTTHK = $i AND tblchitietctdt.maCT = $maCT";
+		$result = $conn -> query($sql);
+
+		while ($row = mysqli_fetch_array($result) ) {
+			$rowCount++;
+			$sheet -> setCellValue('A'.$rowCount, $row['id']);
+			$sheet-> setCellValue('B'.$rowCount, $row['tenMon']);
+			$sheet -> setCellValue('C'.$rowCount, $row['tongSoTC']);
+			$sheet-> setCellValue('D'.$rowCount, $row['soTCLyThuyet']);
+			$sheet -> setCellValue('E'.$rowCount, $row['soTCThuchanh']);
+			if($row['batbuoc'] == 0)
+				$sheet-> setCellValue('F'.$rowCount, "Không");
+			else
+				$sheet-> setCellValue('F'.$rowCount, "Có");
+		}
+		$sheet->setCellValue('A'.($rowCount+1), "Tổng TC:");
+		$sheet->mergeCells("A".($rowCount+1).":B".($rowCount+1));
+		$sheet->setCellValue('C'.($rowCount+1), "=SUM(C$tam:C$rowCount)");
+		$sheet->setCellValue('D'.($rowCount+1), "=SUM(D$tam:D$rowCount)");
+		$sheet->setCellValue('E'.($rowCount+1), "=SUM(E$tam:E$rowCount)");
+		$sheet->setCellValue('F'.($rowCount+1), "");
+		$rowCount = $rowCount +2;
 	}
 	$objWriter =  new PHPExcel_Writer_Excel2007($objExcel);
-	$filename = 'Nganh.xlsx';
+	$filename = 'Hocky.xlsx';
 	$objWriter ->save($filename);
 
 	header('Content-Disposition: attachment; filename="' . $filename . '"');  
@@ -44,6 +87,19 @@ if(isset($_POST['btnExport']))
 </head>
 <body>
 	<form method="POST">
+		<select name="CTDT" data-toggel="tooltip" title="Danh sách các Chương trình Đào tạo">
+			<option>----Chọn Chương trình Đào tạo----</option>
+			<?php
+			$sql = "select * from tblchuongtrinhdt";
+			$rs = $conn-> query($sql);
+			while ($row=$rs -> fetch_assoc()) {
+				?>
+				<option value="<?php echo $row["maCT"]; ?>"><?php echo $row["tenCT"]; ?></option>
+				<?php
+			}   
+			?>
+
+		</select>
 		<button name="btnExport" type = "submit">Export</button>
 	</form>
 </body>
